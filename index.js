69dab4937e92ed6416984103559a08314cee2b88
liteteknoid <64591914+liteteknoid@users.noreply.github.com>
2020-04-30 19:25:18 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var socket = require('socket.io');
+
+
+// App Setup
+var app = express();
+var server = app.listen(4000, function(){
+	console.log('listening to request on port 4000');
+});
+
+// Static Files
+app.use(express.static('public'));
+
+// socket setup
+var io = socket(server);
+
+io.on('connection', function(socket){
+	console.log('made socket connection', socket.id);
+
+// handle chat event
+	socket.on('chat', function(data){
+		io.sockets.emit('chat', data);
+	});
+
+	socket.on('typing', function(data){
+		socket.broadcast.emit('typing', data)
+	});
+
+
+});
